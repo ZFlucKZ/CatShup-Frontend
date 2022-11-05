6c25ca44149f656972c98c61743902b9542dab58
Fluke <guyguy5050@gmail.com>
2022-11-06 03:06:02 +0700

    Auth Page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ForgotPassword from './pages/auth/ForgotPassword';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import ResetPassword from './pages/auth/ResetPassword';
 import Home from './pages/Home/Home';
 
 function App() {
@@ -6,6 +10,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/resetpassword/:resetToken" element={<ResetPassword />} />
       </Routes>
     </BrowserRouter>
   );
