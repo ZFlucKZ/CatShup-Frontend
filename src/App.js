8424cb0c80651ef525fd180dbd90046bc2102d83
Fluke <guyguy5050@gmail.com>
2022-11-08 12:14:53 +0700

    add a product method

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { getLoginStatus } from './services/authService';
 import { SET_LOGIN } from './redux/features/auth/authSlice';
+import AddProduct from './pages/addProduct/AddProduct';
 
 axios.defaults.withCredentials = true;
 
@@ -44,6 +45,16 @@ function App() {
             <Sidebar>
               <Layout>
                 <Dashboard />
+              </Layout>
+            </Sidebar>
+          }
+        />
+        <Route
+          path="/add-product"
+          element={
+            <Sidebar>
+              <Layout>
+                <AddProduct />
               </Layout>
             </Sidebar>
           }
---
+++ src/services/productService.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+
+const API_URL = `${BACKEND_URL}/api/products`;
+
+// Create New Product
+const createNewProduct = async (formData) => {
+  const res = await axios.post(API_URL, formData);
+
+  return res.data;
+};
+
+const productService = {
+  createNewProduct,
+};
+
+export default productService;
